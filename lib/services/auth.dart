9dc8f514e04b15002e8a4fbc22c20ead3f3d5346
AbdullahAbuHaltam <abuhaltamabdullah@gmail.com>
2023-04-21 17:42:29 +0300

    .

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:derbyjo/models/user.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart' show kIsWeb;
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
